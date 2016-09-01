930c48b04b89c0b09cc86b0988ad61ffa18112ac
modesty.zhang <modesty.zhang@servicenow.com>
2016-09-01 10:38:13 -0700

    v0.0.7: update all pages to be stateless pure components

--- src/scripts/pages/contact.js
+++ src/scripts/pages/contact.js
@@ -1,5 +1,4 @@
 "use strict";
-
 import React from 'react';
 
 let pkg = require("../../../package.json");
@@ -10,24 +9,22 @@ import Header from '../components/Header';
 import Main from '../components/Main';
 import Footer from '../components/Footer';
 
-class Contact extends React.Component {
-	render() {
-		let pageTitle = `Contact Us`;
-		return (
-			<html className="no-js" lang="en">
-			<Head title={pageTitle} description={pkg.description} styles={Conf.styles} scripts={Conf.scripts} rel="../"/>
+const Contact = () => {
+	let pageTitle = `Contact Us`;
+	return (
+		<html className="no-js" lang="en">
+		<Head title={pageTitle} description={pkg.description} styles={Conf.styles} scripts={Conf.scripts} rel="../"/>
 
-			<body className='container-fluid'>
-			<Header title={pageTitle} rel="../"/>
-			<Main>
-				<h1>React2Html</h1>
-				<p>email the author: <a href="mailto:modestyz@hotmail.com?subject=About%20CoeProject%20Article">modestyz@hotmail.com</a></p>
-			</Main>
-			<Footer rel="../"/>
-			</body>
-			</html>
-		)
-	}
-}
+		<body className='container-fluid'>
+		<Header title={pageTitle} rel="../"/>
+		<Main>
+			<h1>React2Html</h1>
+			<p>email the author: <a href="mailto:modestyz@hotmail.com?subject=About%20CoeProject%20Article">modestyz@hotmail.com</a></p>
+		</Main>
+		<Footer rel="../"/>
+		</body>
+		</html>
+	);
+};
 
 export default Contact;
