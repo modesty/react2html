7f6e26b0dfe493d07fd5ab66bdc62ca0c7dd4f86
modesty <modestyz@hotmail.com>
2016-09-06 10:50:39 -0700

    v0.1.1: fixed mobile layout, also add client script to handle dropdown auto-close when clicked

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,3 +3,9 @@
 import '../styles/main.scss'; //Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
 
 //import * as utils from './client/util/browser.util';
+
+$( () => {
+	$('.navbar-collapse.collapse.in > ul > li > a').on('click', () => {
+		$('.navbar-toggle').trigger('click');
+	});
+});
